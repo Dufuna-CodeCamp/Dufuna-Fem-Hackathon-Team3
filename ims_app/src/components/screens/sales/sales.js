aaf0d0a0c3e7e8dff6a2268b9cfe4a73bb7f63c6
DevOge <ogejim@gmail.com>
2021-06-25 11:35:23 +0100

    add sales file

--- ims_app/src/components/screens/sales/sales.js
+++ ims_app/src/components/screens/sales/sales.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import Section from '../../includes/section/section';
 
 
 class Sales extends Component {
-    render () {
+    constructor(props) {
+        super(props);
+        this.state = {
+            headers : [
+                {'text' : 'Item Sold', 'value' : "name"},
+                {'text' : 'Sold at', 'value' : "createdAt"},
+                {'text' : 'Sold by', 'value' : "createdBy"},
+            ],
+            sales : [
+                {"name" : "Maggi", "createdAt" : "3/02/2021", "createdBy" : "Lois"},
+                {"name" : "Kitchen Table", "createdAt" : "6/08/2023", "createdBy" : "Taiwo"}
+            ],
+            selectedItem : ""
+        }
+    }
+
+    setSelectedItem= (val) => {
+        this.setState({selectedItem: val.name},
+            alert(`${val.name} was clicked`)
+        );
+    }
+
+    render() {
         return (
-            <div className = "page">
-                <p>Welcome to Sales Page</p>
-            </div>
+            <Section
+                title='Sales'
+                addButtonTitle='Add a new sale'
+                isListPage
+                addSerialNumber
+                addActionBtn
+                onActionClick={this.setSelectedItem}
+                headers={this.state.headers}
+                items={this.state.sales}
+            />
         );
     }
 }
